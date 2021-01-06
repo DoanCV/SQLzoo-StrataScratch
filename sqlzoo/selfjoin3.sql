86acfabdf9f17798056c1721ef211389d804d353
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-01-06 15:09:57 -0500

    Add files via upload

---
+++ sqlzoo/selfjoin3.sql
@@ -0,0 +1,2 @@
+SELECT id, name FROM stops JOIN route ON (stops.id = route.stop) 
+WHERE num = '4' AND company = 'LRT'
---
+++ sqlzoo/selfjoin5.sql
@@ -0,0 +1,3 @@
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON (a.company = b.company AND a.num = b.num)
+WHERE a.stop = 53 AND b.stop IN (SELECT id FROM stops WHERE name = 'London Road')
