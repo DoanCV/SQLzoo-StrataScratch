107ffe4f0e37468ddad095fdee4b699482cded8f
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-01-12 16:38:25 -0500

    Add files via upload

---
+++ stratascratch/9972.sql
@@ -0,0 +1,9 @@
+SELECT employeename, basepay FROM sf_public_salaries
+WHERE jobtitle LIKE '%CAPTAIN%POLICE%' 
+
+/* 
+Wildcard %
+Before string, ends with
+After string, starts with
+Both before and after, any position
+*/
