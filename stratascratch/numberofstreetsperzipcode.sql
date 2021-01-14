63e604b45b2d5af2bec5afecca9d805c73a597bb
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-01-14 18:02:26 -0500

    Delete numberofstreetsperzipcode.sql

--- stratascratch/numberofstreetsperzipcode.sql
+++
@@ -1,3 +0,0 @@
-SELECT COUNT(DISTINCT LOWER(SPLIT_PART(business_address, ' ', 2))), business_postal_code FROM sf_restaurant_health_violations
-GROUP BY business_postal_code
-ORDER BY COUNT(LOWER(SPLIT_PART(business_address, ' ', 2))) DESC, business_postal_code ASC
