e69be5aaa459d97167ec1e577815e9078d3f5fd1
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-03-05 12:27:20 -0500

    Add files via upload

---
+++ stratascratch/10064.sql
@@ -0,0 +1,55 @@
+/* 
+- We have three continents to work with so we will have to add the consumption together to get the total
+    - combine original tables with union all, not just union to not lose rows
+        - alias AS allEnergy
+    - date, SUM(consumption) by each date FROM allEnergy
+        - alias AS totalEnergy
+    - SELECT MAX() FROM totalEnergy
+        - alias as maxEnergy
+- Select the date and max value FROM INNER JOIN ...
+    - there can be ties so returning equal consumptions on different dates is expected
+    - join the maxEnergy and totalEnergy 
+*/
+
+/*
+--First iteration of my solution. 
+--I realize a JOIN is not necessary as well as the maxEnergy table. 
+--A nested select will also work since I am only looking for the max value anyways. 
+
+WITH 
+    allEnergy AS (
+        SELECT * FROM fb_eu_energy
+        UNION ALL
+        SELECT * FROM fb_asia_energy
+        UNION ALL
+        SELECT * FROM fb_na_energy
+        ),
+    totalEnergy AS (
+        SELECT allEnergy.date, SUM(consumption) AS totalConsumption FROM allEnergy
+        GROUP BY allEnergy.date
+        ORDER BY allEnergy.date ASC
+        ),
+    maxEnergy AS (
+        SELECT MAX(totalConsumption) AS maxConsumption FROM totalEnergy
+        )
+SELECT totalEnergy.date, totalEnergy.totalConsumption 
+FROM totalEnergy
+JOIN maxEnergy ON totalEnergy.totalConsumption = maxEnergy.maxConsumption
+*/
+
+WITH 
+    allEnergy AS (
+        SELECT * FROM fb_eu_energy
+        UNION ALL
+        SELECT * FROM fb_asia_energy
+        UNION ALL
+        SELECT * FROM fb_na_energy
+        ),
+    totalEnergy AS (
+        SELECT allEnergy.date, SUM(consumption) AS totalConsumption FROM allEnergy
+        GROUP BY allEnergy.date
+        ORDER BY allEnergy.date ASC
+        )
+SELECT totalEnergy.date, totalEnergy.totalConsumption 
+FROM totalEnergy
+WHERE totalEnergy.totalConsumption = (SELECT MAX(totalConsumption) FROM totalEnergy)
