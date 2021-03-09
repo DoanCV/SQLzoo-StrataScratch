9bf302c13bdeb8eb2a4e8b0f7c1b50cb9875da73
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-03-09 18:45:18 -0500

    Update distancestraveled.sql

--- stratascratch/distancestraveled.sql
+++ stratascratch/distancestraveled.sql
@@ -1,2 +1,3 @@
+-- Problem was removed from the site
 SELECT lyft_rides_log.distance, lyft_users.name FROM lyft_rides_log JOIN lyft_users ON (lyft_users.id = lyft_rides_log.user_id)
 ORDER BY lyft_rides_log.distance DESC LIMIT 10
